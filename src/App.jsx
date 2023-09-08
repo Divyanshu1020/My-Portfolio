42de0c55dc07f944a814000d020c876cac0e7fa8
Divyanshu <divyanshuvishwakarma222@gmail.com>
2023-09-08 22:14:22 +0530

    Creating my portfoliop

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './App.css'
+import Navbar from './Component/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+      
+  )
+}
+
+export default App
